024c7d2cc3acd2137bd4d49da1226e7b3cb5f1ec
TimmyZammit <timmyzammit@icloud.com>
2024-05-11 17:45:59 +0200

    updated url

--- frontend/public/generateRanNum.js
+++ frontend/public/generateRanNum.js
@@ -1,7 +1,7 @@
 document.getElementById('triggerFunction').addEventListener('click', async () => {
     const numberOfCalls = 10000;
     for (let i = 0; i < numberOfCalls; i++) {
-        fetch('YOUR_CLOUD_FUNCTION_URL')
+        fetch('https://europe-west1-assignment-1-v2.cloudfunctions.net/generateRandNum')
             .then(response => response.text())
             .then(data => console.log(`Response from function ${i}: ${data}`))
             .catch(error => console.error('Error calling function:', error));
